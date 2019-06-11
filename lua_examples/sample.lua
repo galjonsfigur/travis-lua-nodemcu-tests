bc9f413088bf8856f69ec3710ece3f05975c21f8
Galion <galion@sdf.org>
2019-06-11 18:29:33 +0200

    And fix it again

--- lua_examples/sample.lua
+++ lua_examples/sample.lua
@@ -7,7 +7,7 @@ local adc1 = ads1115.ads1115(0, ads1115.ADDR_GND)
 -- continuous mode
 adc1:setting(ads1115.GAIN_6_144V, ads1115.DR_128SPS, ads1115.SINGLE_0, ads1115.CONTINUOUS)
 -- read adc result with read()
-local volt, volt_dec, adc, sign = ads1:read()
+local volt, volt_dec, adc, sign = adc1:read()
 print(volt, volt_dec, adc, sign)
 
 end
