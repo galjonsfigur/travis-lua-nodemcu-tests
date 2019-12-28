5dec4a9e3fc3715b8869ae4cba71accd35d124d6
Galion <galion@sdf.org>
2019-12-28 16:52:54 +0100

    Fix the build to check windows CI

--- lua_examples/sample with space.lua
+++ lua_examples/sample with space.lua
@@ -8,7 +8,7 @@ local adc1 = ads1115.ads1115(0, ads1115.ADDR_GND)
 adc1:setting(ads1115.GAIN_6_144V, ads1115.DR_128SPS, ads1115.SINGLE_0, ads1115.CONTINUOUS)
 -- read adc result with read()
 local volt, volt_dec, adc, sign = adc1:read()
-print(volt, volt_dec, sign)
+print(volt, volt_dec, adc, sign)
 
 end
 test()
